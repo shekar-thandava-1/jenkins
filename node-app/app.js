47463f4396009970dccb65cad04330ef6e86e8d6
Shekar Thandava <cacophonous.bard@gmail.com>
2025-12-17 19:37:06 +0530

    correcting working dir

--- node-app/app.js
+++ node-app/app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/hello', (req, res) => {
-  res.send('Hello, World! Welcome!!\n');
+  res.send('Hello, World! Welcome To The Jungle!!\n');
 });
 
 app.listen(port, () => {
