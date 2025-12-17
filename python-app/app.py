56b313160a10f1ea7dd98275644c7cd558d12a6c
Shekar Thandava <cacophonous.bard@gmail.com>
2025-12-18 00:07:11 +0530

    correcting working dir

--- python-app/app.py
+++ python-app/app.py
@@ -5,9 +5,9 @@ app = Flask(__name__)
 
 @app.route('/hello')
 def hello():
-    return 'Hello, World! Welcome!!\n'
+    return 'Hello, World! Welcome Python!!\n'
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 3000))
+    port = int(os.environ.get('PORT', 3500))
     app.run(debug=True, host='0.0.0.0', port=port)
 
